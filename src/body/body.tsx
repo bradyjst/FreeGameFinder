8d19a122ef34d8bb4cffce148fd589cb62c72c52
Brady Stacey <bradyjstacey@gmail.com>
2022-11-01 00:07:33 -0700

    Made the buttons work, still calls api everytime tho. Also removed some dummy code.

--- src/body/body.tsx
+++ src/body/body.tsx
@@ -1,12 +1,16 @@
 /* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { Game } from "../game/Game";
 import { getGames } from "../api";
 import { useFela } from "react-fela";
 import { BodyContainer } from "./body.fela";
 
-export const Body: React.FC = () => {
-	const [games, setGames] = useState([]);
+interface BodyProps {
+	genre: any;
+}
+
+export const Body: React.FC<BodyProps> = ({ genre }) => {
+	const [games, setGames] = useState<any[]>([]);
 
 	useEffect(() => {
 		getGames().then((data) => {
@@ -14,14 +18,18 @@ export const Body: React.FC = () => {
 		});
 	}, []);
 
+	const filteredGames = useMemo(() => {
+		return games.filter((game) => game.genre === genre);
+	}, [games, genre]);
+
 	console.log(games);
 
 	const { css } = useFela();
 
 	return (
 		<div className={css(BodyContainer)}>
-			{games.slice(0, 99).map((games, i) => (
-				<Game games={games} key={i} />
+			{filteredGames.slice(0, 99).map((game, i) => (
+				<Game game={game} key={i} />
 			))}
 		</div>
 	);
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -3,24 +3,30 @@ import { HeaderContainer } from "./Header.fela";
 import { useFela } from "react-fela";
 import { Buttons } from "./Header.fela";
 
-interface HeaderProps {
-	genre: string;
+export interface HeaderProps {
+	setGenre: any;
 }
 
-export const Header: React.FC<HeaderProps> = ({ genre }) => {
+export const Header: React.FC<HeaderProps> = ({ setGenre }) => {
 	const { css } = useFela();
+
+	const getGenre = (g: string) => {
+		//Hahah! G string
+		setGenre(g);
+		console.log("buttonworks!");
+	};
 
 	return (
 		<div className={css(HeaderContainer)}>
 			<h1>Find a game!</h1>
 			<div className={css(Buttons)}>
-				<button>Fighting</button>
-				<button>Strategy</button>
-				<button>MMORPG</button>
-				<button>Shooter</button>
-				<button>Adventure</button>
-				<button>Sports</button>
-				<button>Card Game</button>
+				<button onClick={() => getGenre("Shooter")}>Shooter</button>
+				<button onClick={() => getGenre("Strategy")}>Strategy</button>
+				<button onClick={() => getGenre("MMORPG")}>MMORPG</button>
+				<button onClick={() => getGenre("Fighting")}>Fighting</button>
+				<button onClick={() => getGenre("Moba")}>Moba</button>
+				<button onClick={() => getGenre("Sports")}>Sports</button>
+				<button onClick={() => getGenre("Card Game")}>Card Game</button>
 			</div>
 		</div>
 	);
